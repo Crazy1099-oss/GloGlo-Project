8986f553b7aa12ad9db21db78d18d8e3f4576ada
Klimenko Danil <Crazy_1099@mail.ru>
2026-04-16 18:26:03 +0300

    Добавленние переменных в script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+alert("Hello world!")
+console.log("Hello world!")
+
+
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+
+title = "Яблоко";
+screens = 25;
+screenPrice = 9999;
+rollback = true;
+fullPrice = screens * screenPrice;
+adaptive = false;
+
+console.log("title:", title,
+    "screens:", screens,
+    "screenPrice:", screenPrice,
+    "rollback:", rollback,
+    "fullPrice:", fullPrice,
+    "adaptive:", adaptive);
