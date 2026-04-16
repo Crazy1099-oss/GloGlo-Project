168d75b29d432c812e3bfb8ee8cb018aa3a01850
Klimenko Danil <Crazy_1099@mail.ru>
2026-04-16 19:15:39 +0300

    Добавление фала script.js втрого урока

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-alert("Hello world!")
-console.log("Hello world!")
-
-
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
-
-title = "Яблоко";
-screens = 25;
-screenPrice = 9999;
-rollback = true;
-fullPrice = screens * screenPrice;
+console.log("----------Первое задание----------")
+title = "GloGlo Project";
+screens = "Простые, Сложные, Интерактивные";
+screenPrice = 1294889213757812;
+rollback = 25;
+fullPrice = 250000;
 adaptive = false;
 
 console.log("title:", title,
-    "screens:", screens,
-    "screenPrice:", screenPrice,
-    "rollback:", rollback,
-    "fullPrice:", fullPrice,
-    "adaptive:", adaptive);
+    "| screens:", screens,
+    "| screenPrice:", screenPrice,
+    "| rollback:", rollback,
+    "| fullPrice:", fullPrice,
+    "| adaptive:", adaptive);
+
+
+
+console.log("----------Второе задание----------")
+console.log("title:",typeof title,"| fullPrice:",typeof fullPrice,"| adaptive:",typeof adaptive)
+console.log("Узнаем длинну строки:", screens.length)
+console.log("Стоимость верстки экранов:", screenPrice,"рублей")
+console.log("Стоимость разработки сайта:", fullPrice,"рублей")
+
+screens = screens.toLowerCase();
+let screensArray = screens.split(', ');
+console.log("Массив:",screensArray);
+
+let rollbackPercent = fullPrice * (rollback / 100);
+console.log('Процент отката посреднику за работу:', rollbackPercent);
