5c93d5770d64ec2e8d3d539ae10c25c645b20708
Klimenko Danil <Crazy_1099@mail.ru>
2026-04-20 21:07:28 +0300

    доделал 04 усложненное дз

--- script02.js
+++ script02.js
@@ -1,11 +1,16 @@
 function editTextStr(str) {
+    if (typeof str !== 'string') {
+        alert('Передано число')
+        return;
+    }
+
     let textStr = str.trim();
 
     if (textStr.length > 30) {
         textStr = textStr.slice(0, 30) + ('...')
     }
-    
-    return textStr
+
+    return textStr;
 }
 
 let endTextStr = editTextStr('    Мы боимся темноты, монстров, скрипа дверей в пустом доме.     ')
