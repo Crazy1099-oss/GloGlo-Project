0e14c19f9ed08557a77c88805d596b31206be034
Klimenko Danil <Crazy_1099@mail.ru>
2026-04-20 15:25:27 +0300

    выполнение урока по js

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-const n = promnt("как называется ваш проект?")
+const title = prompt("1. Как называется ваш проект?");
+console.log(title);
+
+const screens = prompt("Какие типы экранов нужно разработать?");
+console.log(screens);
+
+const screenPrice = +prompt("Сколько будет стоить данная работа?");
+console.log(screenPrice);
+
+const adaptive = confirm("Нужен ли адаптив на сайте?");
+console.log(adaptive);
+
+const service1 = prompt("2. Какой дополнительный тип услуги нужен?");
+console.log(service1);
+
+const servicePrice1 = +prompt("Сколько это будет стоить?");
+console.log(servicePrice1);
+
+const service2 = prompt("3. Какой дополнительный тип услуги нужен?");
+console.log(service2);
+
+const servicePrice2 = +prompt("Сколько это будет стоить?");
+console.log(servicePrice2);
+
+
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = 0;
+console.log("Итого:", fullPrice);
+
+
+switch (fullPrice >= 0) {
+    case fullPrice >= 30000:
+        servicePercentPrice = fullPrice - (fullPrice * 10 / 100)
+        console.log("Итого:", servicePercentPrice)
+        break;
+    case fullPrice >= 15000 && fullPrice < 30000:
+        servicePercentPrice = fullPrice - (fullPrice * 5 / 100)
+        console.log("Итого:", servicePercentPrice)
+        break;
+    case fullPrice < 15000 && fullPrice < 0:
+        console.log("Скидка не предусмотрена")
+        break;
+    case fullPrice == 0:
+        console.log("Заказ на сумму 0 рублей невозможен")
+        break;
+}
