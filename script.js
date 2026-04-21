7b38aec98e5a761ba81a088af12aadd73df8a5a8
Klimenko Danil <Crazy_1099@mail.ru>
2026-04-21 20:43:31 +0300

    Добавление 5го дз

---
+++ script.js
@@ -0,0 +1,65 @@
+let title = prompt("1. Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать?");
+
+let screenPrice;
+let servicePrice1;
+let servicePrice2;
+
+let allServicePrices = 0;
+let fullPrice = 0;
+
+do {
+    screenPrice = +prompt("Сколько будет стоить данная работа?");
+} while (isNaN(screenPrice) || screenPrice <= 0);
+
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+let service1 = prompt("2. Какой дополнительный тип услуги нужен?");
+
+do {
+    servicePrice1 = +prompt("Сколько это будет стоить?");
+} while (isNaN(servicePrice1) || servicePrice1 < 0);
+
+let service2 = prompt("3. Какой дополнительный тип услуги нужен?");
+
+do {
+    servicePrice2 = +prompt("Сколько это будет стоить?");
+} while (isNaN(servicePrice2) || servicePrice2 < 0);
+
+function getAllServicePrices() {
+    allServicePrices = servicePrice1 + servicePrice2;
+    console.log("Сумма всех дополнительных услуг:", allServicePrices);
+}
+
+function getFullPrice() {
+    fullPrice = screenPrice + allServicePrices;
+    console.log("Сумма стоимости верстки и стоимости дополнительных услуг:", fullPrice);
+}
+
+function getTitle() {
+    return title.toUpperCase();
+}
+
+function getServicePercentPrices() {
+    let discountedPrice;
+    if (fullPrice >= 30000) {
+        discountedPrice = fullPrice - (fullPrice * 0.10);
+        console.log("Скидка 10%", discountedPrice);
+    } else if (fullPrice >= 15000 && fullPrice < 30000) {
+        discountedPrice = fullPrice - (fullPrice * 0.05);
+        console.log("Скидка 5%", discountedPrice);
+    } else if (fullPrice > 0 && fullPrice < 15000) {
+        discountedPrice = fullPrice;
+        console.log("Скидка не предусмотрена");
+    } else {
+        discountedPrice = 0;
+        console.log("Заказ на сумму 0 рублей невозможен");
+    }
+    return discountedPrice;
+}
+
+getAllServicePrices();
+getFullPrice();
+console.log("Название проекта:", getTitle());
+
+console.log("Типы экранов для разработки: ", screens);
+console.log("Итоговая стоимость с учётом скидки: ", getServicePercentPrices());
