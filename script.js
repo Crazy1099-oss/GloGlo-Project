4de71d7e985e9d70263f49f5a94c1c898bcba3bf
Klimenko Danil <Crazy_1099@mail.ru>
2026-05-05 05:57:40 +0300

    добавление нового задания 16го урока

---
+++ script.js
@@ -0,0 +1,15 @@
+class First {
+  hello() {
+    console.log("Привет я метод родителя!");
+  }
+}
+
+class Second extends First {
+  hello() {
+    super.hello();
+    console.log("А я наследуемый метод!");
+  }
+}
+
+const object = new Second();
+object.hello();
